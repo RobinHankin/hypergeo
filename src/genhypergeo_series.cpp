e2c23143af356fbb186b30a54dbb819c79e17903
RobinHankin <hankin.robin@gmail.com>
2018-08-31 20:37:44 +1200

    whitespace

--- src/genhypergeo_series.cpp
+++ src/genhypergeo_series.cpp
@@ -21,7 +21,7 @@ bool isgood(const arma::cx_vec x, const double tol)
 // [[Rcpp::export]]
 arma::cx_vec genhypergeo_series( const arma::cx_vec U,
                                  const arma::cx_vec L,
-                                 arma::cx_vec z,
+                                       arma::cx_vec z,
                                  const double tol,
                                  const int maxiter,
                                  const bool check_mod,
@@ -76,7 +76,8 @@ arma::cx_vec genhypergeo_series( const arma::cx_vec U,
 
 
 // [[Rcpp::export]]
-Rcpp::ComplexVector genhypergeo_series_cpp(const Rcpp::NumericVector & real_U,
+Rcpp::ComplexVector genhypergeo_series_cpp(
+				      const Rcpp::NumericVector & real_U,
                                       const Rcpp::NumericVector & imag_U,
                                       const Rcpp::NumericVector & real_L,
                                       const Rcpp::NumericVector & imag_L,
